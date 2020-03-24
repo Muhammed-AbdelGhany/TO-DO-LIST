fb65112625f07986841051b82a4ffd307d45c88f
muhammed <muhammedabdelghany5297@gmail.com>
2020-03-24 05:27:52 -0700

    table view is done

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -23,7 +23,19 @@ class ToDoViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
         
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+                       tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
     }
 
 }
