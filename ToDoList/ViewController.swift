bc7dc437d7f65aa4869249ca9f1878959c9c02ae
muhammed <muhammedabdelghany5297@gmail.com>
2020-03-24 05:26:21 -0700

    table view done

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoViewController: UITableViewController {
+    
+    let toDoArray = ["Finish some work", "Rank up in league", "Keep it up"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 
 }
 
